93cc50777d28ceb4a1ea3aa1310b992d657427ee
sangypark0324 <pa324@naver.com>
2024-10-18 08:58:15 +0900

    getBrandWithLowestTotalPrice 테스트 추가

--- pricing-api/src/test/java/com/musinsa/pricing/service/LowestPriceServiceTest.java
+++ pricing-api/src/test/java/com/musinsa/pricing/service/LowestPriceServiceTest.java
@@ -1,7 +1,10 @@
 package com.musinsa.pricing.service;
 
+import com.musinsa.pricing.domain.projection.BrandTotalLowestPriceProjection;
 import com.musinsa.pricing.domain.projection.LowestPriceProjection;
+import com.musinsa.pricing.model.response.LowestBrandResult;
 import com.musinsa.pricing.model.response.LowestPricePerCategoryResult;
+import com.musinsa.pricing.repository.BrandRepository;
 import com.musinsa.pricing.repository.CategoryLowestPriceRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -20,6 +23,9 @@ import static org.mockito.Mockito.when;
 class LowestPriceServiceTest {
     @Mock
     private CategoryLowestPriceRepository categoryLowestPriceRepository;
+
+    @Mock
+    private BrandRepository brandRepository;
 
     @InjectMocks
     private LowestPriceService lowestPriceService;
@@ -53,5 +59,36 @@ class LowestPriceServiceTest {
         assertEquals(2, result.getLowestPricePerCategory().size());
         assertEquals(new BigDecimal("15000"), result.getTotalPrice());
         
+    }
+
+    @Test
+    public void 단일_브랜드의_모든카테고리에서_최저가상품을_구매할때의_최저가브랜드명과_전체금액을_구한다() {
+        // Given
+        final String BRAND_NAME = "A";
+        BrandTotalLowestPriceProjection mockBrandProjection = mock(BrandTotalLowestPriceProjection.class);
+        when(mockBrandProjection.getBrandId()).thenReturn(1L);
+        when(mockBrandProjection.getBrandName()).thenReturn(BRAND_NAME);
+        when(mockBrandProjection.getTotalLowestPrice()).thenReturn(BigDecimal.valueOf(10000));
+
+        LowestPriceProjection mockLowestPriceProjection1 = mock(LowestPriceProjection.class);
+        when(mockLowestPriceProjection1.getCategoryName()).thenReturn("상의");
+        when(mockLowestPriceProjection1.getLowestPrice()).thenReturn(BigDecimal.valueOf(6000));
+
+        LowestPriceProjection mockLowestPriceProjection2 = mock(LowestPriceProjection.class);
+        when(mockLowestPriceProjection2.getCategoryName()).thenReturn("하의");
+        when(mockLowestPriceProjection2.getLowestPrice()).thenReturn(BigDecimal.valueOf(4000));
+
+        List<LowestPriceProjection> mockLowestPriceProjections = List.of(mockLowestPriceProjection1, mockLowestPriceProjection2);
+
+        when(brandRepository.findBrandWithLowestTotalPrice()).thenReturn(mockBrandProjection);
+        when(brandRepository.findLowestPriceProductsByBrandId(1L)).thenReturn(mockLowestPriceProjections);
+
+
+        // When
+        LowestBrandResult result = lowestPriceService.getBrandWithLowestTotalPrice();
+
+        // Then: Verify the result
+        assertEquals(BRAND_NAME, result.getLowestPriceBrandInfo().getBrandName());
+        assertEquals(BigDecimal.valueOf(10000), result.getLowestPriceBrandInfo().getTotalPrice());
     }
 }
